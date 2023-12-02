7dc27b887541eea16c6276897f4f771a6f665f46
Sahil Chavan <142314251+sahil-gpm@users.noreply.github.com>
2023-12-02 16:51:48 +0530

    Updated Firebase configurations

--- src/FirebaseConfig/Firebase.jsx
+++ src/FirebaseConfig/Firebase.jsx
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADt8A10ad1Dzzn4GBRpKaHtcRuFczfC2I",
-  authDomain: "newsfeed-cd37a.firebaseapp.com",
-  projectId: "newsfeed-cd37a",
-  storageBucket: "newsfeed-cd37a.appspot.com",
-  messagingSenderId: "883666310973",
-  appId: "1:883666310973:web:2681f0ab3203d3c91dd754"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
